6432eb7cdf04d593371d38754208fae90c9eb164
Yasufumi Nakamoto <y_nakamoto@dreamarts.co.jp>
2023-06-18 11:59:51 +0900

    add index

---
+++ app/mysql/mysql_migration/1_user.sql
@@ -0,0 +1,3 @@
+ALTER TABLE user ADD INDEX user_index (entry_date ASC, kana ASC);
+ALTER TABLE user ADD INDEX usericon_index (user_icon_id);
+ALTER TABLE user ADD INDEX mailpass_index (mail, password);
---
+++ app/mysql/mysql_migration/2_other.sql
@@ -0,0 +1,3 @@
+ALTER TABLE match_group_member ADD INDEX match_user_index (user_id);
+-- ALTER TABLE user ADD FULLTEXT INDEX ft_idx(user_name,goal,mail,kana) WITH PARSER ngram;
+ALTER TABLE skill_member ADD INDEX skillid_index (user_id);
