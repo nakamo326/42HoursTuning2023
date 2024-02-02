068b181c508f8779dad830cbd9e891b08f544961
Ynakamot <nakamo326@gmail.com>
2024-02-02 11:50:48 +0900

    add index

--- app/mysql/mysql_migration/1_user.sql
+++ app/mysql/mysql_migration/1_user.sql
@@ -1,4 +1,5 @@
 ALTER TABLE user ADD INDEX user_index (entry_date ASC, kana ASC);
 ALTER TABLE user ADD INDEX usericon_index (user_icon_id);
+ALTER TABLE user ADD INDEX office_id_index (office_id);
 ALTER TABLE user ADD INDEX emp_index (employee_id);
 ALTER TABLE user ADD INDEX mailpass_index (mail, password);
--- app/mysql/mysql_migration/4_match.sql
+++ app/mysql/mysql_migration/4_match.sql
@@ -1,3 +1,4 @@
 -- DELETE FROM match_group_member where match_group_id IN (SELECT match_group_id FROM match_group WHERE match_group_name = "S")
 
 ALTER TABLE match_group_member ADD INDEX match_user_index (match_group_name);
+ALTER TABLE match_group_member ADD INDEX match_user_id_index (match_group_name);
