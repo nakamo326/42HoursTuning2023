171b2f512afa0a2bde50d55658be788f4cab8b58
Ynakamot <nakamo326@gmail.com>
2024-02-02 11:32:27 +0900

    fix index

--- app/mysql/mysql_migration/3_fulltext.sql
+++ app/mysql/mysql_migration/3_fulltext.sql
@@ -1,8 +1,8 @@
-ALTER TABLE `user` ADD FULLTEXT INDEX idx_ngram_user_name (`user_name`) WITH PARSER ngram;
-ALTER TABLE `user` ADD FULLTEXT INDEX idx_ngram_kana (`kana`) WITH PARSER ngram;
-ALTER TABLE `user` ADD FULLTEXT INDEX idx_ngram_mail (`mail`) WITH PARSER ngram;
-ALTER TABLE `user` ADD FULLTEXT INDEX idx_ngram_goal (`goal`) WITH PARSER ngram;
+ALTER TABLE user ADD FULLTEXT INDEX idx_ngram_user_name (user_name) WITH PARSER ngram;
+ALTER TABLE user ADD FULLTEXT INDEX idx_ngram_kana (kana) WITH PARSER ngram;
+ALTER TABLE user ADD FULLTEXT INDEX idx_ngram_mail (mail) WITH PARSER ngram;
+ALTER TABLE user ADD FULLTEXT INDEX idx_ngram_goal (goal) WITH PARSER ngram;
 
-ALTER TABLE `department` ADD FULLTEXT INDEX idx_ngram_department_name (`department_name`) WITH PARSER ngram;
-ALTER TABLE `role` ADD FULLTEXT INDEX idx_ngram_role_name (`role_name`) WITH PARSER ngram;
-ALTER TABLE `office` ADD FULLTEXT INDEX idx_ngram_office_name (`office_name`) WITH PARSER ngram;
+ALTER TABLE department ADD FULLTEXT INDEX idx_ngram_department_name (department_name) WITH PARSER ngram;
+ALTER TABLE role ADD FULLTEXT INDEX idx_ngram_role_name (role_name) WITH PARSER ngram;
+ALTER TABLE office ADD FULLTEXT INDEX idx_ngram_office_name (office_name) WITH PARSER ngram;
